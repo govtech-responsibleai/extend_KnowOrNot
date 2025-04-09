9aa09ffe8807e59cb291bf5314c3e3fee1faccd3
pradyuprasad <pradyu.sp@gmail.com>
2025-04-09 12:22:44 +0800

    feat: add AtomicFact and AtomicFactDocument
    - AtomicFact is the fact we will send to the LLM to create a question
    - AtomicFactDocument is the document which contains these
    - The str of the document does not include the sources from each fact to
      avoid confusing the LLM when it cites sources

--- src/knowornot/DataManager/models.py
+++ src/knowornot/DataManager/models.py
@@ -10,7 +10,22 @@ class SplitSourceDocument(BaseModel):
     sentences: List[Sentence]
 
     def __str__(self) -> str:
-        result = ""
+        result = "Source document: \n"
         for i, sentence in enumerate(self.sentences):
             result += f"{i}: {sentence.text}\n"
+        return result.strip()
+
+
+class AtomicFact(BaseModel):
+    fact_text: str
+    source_citation: int
+
+
+class AtomicFactDocument(BaseModel):
+    fact_list: List[AtomicFact]
+
+    def __str__(self) -> str:
+        result = "Atomic facts document: \n"
+        for i, fact in enumerate(self.fact_list):
+            result += f"{i}: {fact.fact_text}\n"
         return result.strip()
